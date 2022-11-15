b042f9017708026177856abb758982153ac7d798
eszdevstar <devstaresz@gmail.com>
2022-11-15 17:25:07 -0300

    create initial functions add, subtract, multiply, divide and operator

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 
+const add = (a, b) =>  a + b;
 
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
+
+const operator = (op, a, b) => {
+    if(op === '+') {
+        return add(a, b);
+    } else if(op === '-') {
+        return subtract(a, b);
+    } else if(op === '*') {
+        return multiply(a, b);
+    } else if(op === '/') {
+        return divide(a, b);
+    } else {
+        return 'needs a operator';
+    }
+}
