9cba33bfc7ac4306ad6d5d113ac6f29c25f5f3f6
eszdevstar <devstaresz@gmail.com>
2022-11-16 00:01:19 -0300

    Remake the style of the calculator

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 
+// Operations
 const add = (a, b) =>  a + b;
-
 const subtract = (a, b) => a - b;
-
 const multiply = (a, b) => a * b;
-
 const divide = (a, b) => a / b;
-
 const operator = (op, a, b) => {
     if(op === '+') {
         return add(a, b);
@@ -20,3 +17,36 @@ const operator = (op, a, b) => {
         return 'needs a operator';
     }
 }
+
+// Logic
+const buttons = document.querySelectorAll('.button');
+const displayContainer = document.getElementById('display-container');
+const erase = () => displayContainer.innerHTML = '';
+
+buttons.forEach((button) => {
+    // change button color
+    button.addEventListener('mouseover', () => {
+        button.style.color = 'rgb(124 89 32/1)'
+    });
+    button.addEventListener('mouseout', () => {
+        button.style.color = 'rgb(209 213 219/1)'
+    });
+});
+
+buttons.forEach((button) =>{
+    // Listener to display Numbers
+    button.addEventListener('click', () =>{
+        if(button.textContent === 'Clear'){
+            erase();
+        }else{
+            document.getElementById('display-container').innerHTML +=  (`<div id="${button.textContent}" class="display">${button.textContent}</div>`);
+        }
+
+        const number = document.querySelectorAll('.display');
+        
+        
+    });
+});
+
+
+
